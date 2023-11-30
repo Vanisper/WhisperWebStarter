dcab8e0035e39cfca87590358e3137436a45ffbb
Vanisper <273266469@qq.com>
2023-11-30 13:10:18 +0800

    fix: 修复github工作流构建的页面无法正常显示

--- config/vite.config.base.ts
+++ config/vite.config.base.ts
@@ -7,6 +7,7 @@ import configArcoStyleImportPlugin from './plugin/arcoStyleImport';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: './',
     plugins: [
         vue(),
         vueJsx(),
